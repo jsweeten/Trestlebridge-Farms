6160194dad767e1b04fa1716e85d4ba3f761617f
Jacob Thomas <jacgretho@gmail.com>
2022-12-09 13:54:09 -0600

    bunch of stuff

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -12,29 +12,38 @@ namespace Trestlebridge.Actions
         {
             Utils.Clear();
 
-            for (int i = 0; i < farm.GrazingFields.Count; i++)
+            if (farm.GrazingFields.Count > 0)
             {
-                int animalCount = farm.GrazingFields[i].CountAnimals();
+                for (int i = 0; i < farm.GrazingFields.Count; i++)
+                {
+                    int animalCount = farm.GrazingFields[i].CountAnimals();
 
 
-                Console.WriteLine($"{i + 1}. Grazing Field ({animalCount} animals).");
+                    Console.WriteLine($"{i + 1}. Grazing Field ({animalCount} animals).");
+                }
+
+                Console.WriteLine();
+
+                // How can I output the type of animal chosen here?
+                Console.WriteLine($"Place the {animal.GetType().Name} where?");
+
+                Console.Write("> ");
+                int choice = Int32.Parse(Console.ReadLine());
+
+                farm.GrazingFields[choice - 1].AddResource(animal);
+
+                /*
+                    Couldn't get this to work. Can you?
+                    Stretch goal. Only if the app is fully functional.
+                 */
+                // farm.PurchaseResource<IGrazing>(animal, choice);
             }
-
-            Console.WriteLine();
-
-            // How can I output the type of animal chosen here?
-            Console.WriteLine($"Place the animal where?");
-
-            Console.Write("> ");
-            int choice = Int32.Parse(Console.ReadLine());
-
-            farm.GrazingFields[choice - 1].AddResource(animal);
-
-            /*
-                Couldn't get this to work. Can you?
-                Stretch goal. Only if the app is fully functional.
-             */
-            // farm.PurchaseResource<IGrazing>(animal, choice);
+            else
+            {
+                Console.WriteLine("No grazing fields present.  Please add a grazing field and try again.");
+                Console.WriteLine("Press enter to continue.");
+                Console.ReadLine();
+            }
 
         }
     }
